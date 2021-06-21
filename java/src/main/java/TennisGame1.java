9d6f1b1990d1bfc86ea299426afed537e4917d5c
“Ashwin <Ashwin.Joshi@cdk.com>
2021-06-21 15:07:03 +0530

    r- removed temp variable.

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -25,11 +25,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String getScoreDefault() {
-        String score = "";
-        score += getScore(m_score1);
-        score += "-";
-        score += getScore(m_score2);
-        return score;
+        return getScore(m_score1) + "-" + getScore(m_score2);
     }
 
 
@@ -38,13 +34,11 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String getScoreAdvantage() {
-        String score;
         int minusResult = m_score1-m_score2;
-        if (minusResult==1) score ="Advantage player1";
-        else if (minusResult ==-1) score ="Advantage player2";
-        else if (minusResult>=2) score = "Win for player1";
-        else score ="Win for player2";
-        return score;
+        if (minusResult==1) return "Advantage player1";
+        if (minusResult ==-1) return "Advantage player2";
+        if (minusResult>=2) return "Win for player1";
+        return "Win for player2";
     }
 
     private String getScoreForEqual(final int tempScore, final String postfix, final String defaultScoreText) {
