d88911f84d930793b107a5f1ff84785f87538d79
Joshi, Ashwin <ashwin.joshi@sincrodigital.com>
2021-08-18 14:38:57 +0530

    r- name player variables final.

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -1,10 +1,12 @@
+import static java.lang.Math.abs;
+
 public class TennisGame2 implements TennisGame
 {
     public int P1point = 0;
     public int P2point = 0;
 
-    private String player1Name;
-    private String player2Name;
+    final private String player1Name;
+    final private String player2Name;
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
@@ -22,7 +24,7 @@ public class TennisGame2 implements TennisGame
     private String getScoreAdvantageOrWin(int p1point, int p2point) {
         final int pointDiff = p1point - p2point;
         String player = pointDiff > 0 ? player1Name : player2Name;
-        String text = Math.abs(pointDiff) >= 2 ? "Win for " : "Advantage ";
+        String text = abs(pointDiff) >= 2 ? "Win for " : "Advantage ";
         return text + player;
     }
     private static String getScoreForEqual(final int point) {
