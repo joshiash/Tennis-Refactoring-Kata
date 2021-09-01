3d9f322e9e647126ba0ac78d5183dc27dcfced1b
Rahul Dongare <Rahul.Dongare@sincrodigital.com>
2021-09-01 14:52:59 +0530

    r -> extracted isGameOn method

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -1,6 +1,7 @@
 
 public class TennisGame3 implements TennisGame {
-    
+
+    private static final String[] scoreText = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
     private int p2;
     private int p1;
     private String p1N;
@@ -12,19 +13,19 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        if (isGameOn(p1, p2)) {
+            return (p1 == p2) ? scoreText[p1] + "-All" : scoreText[p1] + "-" + scoreText[p2];
         } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
+            if (p1 == p2) return "Deuce";
+            String s = p1 > p2 ? p1N : p2N;
             return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
-    
+
+    private static boolean isGameOn(final int p1, final int p2) {
+        return p1 < 4 && p2 < 4 && !(p1 + p2 == 6);
+    }
+
     public void wonPoint(String playerName) {
         if (playerName == "player1")
             this.p1 += 1;
